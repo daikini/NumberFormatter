bafc6f6472eee1140b280984c3c861c914e5e4de
Jonathan Younger <jonathan@daikini.com>
2014-09-26 07:54:25 -0600

    Simply tests

--- NumberFormatterTests/NumberFormatterTests.m
+++ NumberFormatterTests/NumberFormatterTests.m
@@ -8,38 +8,19 @@
 
 #import "NumberFormatterTests.h"
 
-static double const kMilesToMetersCoefficient = 1609.344;
-static double const kMetersToMilesCoefficient = 0.000621371192;
-
-@interface NumberFormatterTests()
-@property (strong, nonatomic) NSNumberFormatter *formatter;
-@property (strong, nonatomic) NSNumber *number;
-@end
-
 @implementation NumberFormatterTests
 
-- (void)setUp {
-    [super setUp];
-    
-    self.formatter = [[NSNumberFormatter alloc] init];
-    [self.formatter setUsesSignificantDigits:NO];
-    [self.formatter setMaximumSignificantDigits:2];
-    [self.formatter setMaximumFractionDigits:1];
-    
-    double number = 292.0;
-    double numberInMeters = number * kMilesToMetersCoefficient;
-    double numberInMiles = numberInMeters * kMetersToMilesCoefficient;
-    self.number = @(numberInMiles);
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testFormattedNumberShouldEqualOriginalNumber
+- (void)testFormattedNumberShouldEqualOriginalNumberWhenUsingTwoSignificantDigits
 {
-    STAssertEqualObjects(@"292", [self.formatter stringFromNumber:self.number], @"Formatted number should equal original number");
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    [formatter setUsesSignificantDigits:NO];
+    [formatter setMaximumFractionDigits:1];
+    
+    [formatter setMaximumSignificantDigits:2];
+    
+    NSNumber *number = @(291.99);
+    
+    STAssertEqualObjects(@"292", [formatter stringFromNumber:number], nil);
 }
 
 @end
